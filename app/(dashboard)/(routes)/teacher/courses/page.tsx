6638fb6fb8a162f2d3f777b518fd9a15792ee420
YoussefNesafe <ynessafe@gmail.com>
2023-10-22 18:31:07 +0400

    init: create course page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from '@/components/ui/button'
+import { PlusCircle } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
-const page = () => {
+const CoursesPage = () => {
   return (
-    <div>courses page</div>
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>
+          <PlusCircle className="w-4 h-4 mr-2" />
+          New course
+        </Button>
+      </Link>
+    </div>
   )
 }
 
-export default page
+export default CoursesPage 
